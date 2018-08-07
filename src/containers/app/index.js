4a63e37ae51b50df0af05c0e746334f268b6e037
Wes Christiansen <wes.christiansen@liveauctioneers.com>
2018-08-06 22:39:53 -0700

    basic setup

---
+++ src/containers/app/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from '../home';
+import About from '../about';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <Link to="/">Home</Link>
+                    <Link to="/about-us">About</Link>
+                </header>
+                <main>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about-us" component={About} />
+                </main>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'connected-react-router';
+import store, {history} from './store';
+import App from './containers/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+
+const target = document.querySelector('#root')
+
+render(
+    <Provider store={store}>
+    <ConnectedRouter history={history}>
+        <div>
+            <App/>
+        </div>
+    </ConnectedRouter>
+</Provider>, target);
